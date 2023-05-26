4f808c552a3964888bf42662a8631cb3a7d86247
lokilow <lokilow@pm.me>
2023-05-26 09:02:12 -0400

    display active food trucks

--- lib/food.ex
+++ lib/food.ex
@@ -1,9 +1,19 @@
 defmodule Food do
   @moduledoc """
-  Food keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  Food is a web app that displays information about the food trucks in San Francisco
   """
+  alias Food.Repo
+  import Ecto.Query
+
+  @doc """
+  Return active food trucks.
+
+  This is a naive approach in the essence of time, and it may be the case
+  that there are counfounding variables with this approach
+  such that the food trucks returned here are not all active, or not all
+  of the active food trucks.
+  """
+  def active_food_trucks() do
+    Food.FoodTruck |> where(status: "APPROVED") |> order_by(asc: :applicant) |> Repo.all()
+  end
 end
--- lib/food_web/live/page_live.ex
+++ lib/food_web/live/page_live.ex
@@ -1,9 +1,26 @@
 defmodule FoodWeb.PageLive do
-  use Phoenix.LiveView
+  use FoodWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    trucks = Food.active_food_trucks()
+    {:ok, stream(socket, :trucks, trucks)}
+  end
 
   def render(assigns) do
     ~H"""
-    <h1 class="text-5xl text-center font-bold my-16">San Francisco Food Trucks</h1>
+    <h1 class="my-16 text-5xl font-bold text-center">San Francisco Food Trucks</h1>
+    <table class="mx-auto">
+      <tbody id="trucks" phx-update="stream">
+        <tr :for={{_dom_id, truck} <- @streams.trucks} class="flex border-b border-cool-gray-300">
+          <td class="w-40"><%= truck.applicant %></td>
+          <td class="w-64 ml-4"><%= truck.food_items %></td>
+          <td class="ml-4 w-72"><%= truck.location_description %></td>
+          <td class="ml-4">
+            <a href={"#{truck.schedule}"}><.icon name="hero-calendar-solid" /></a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     """
   end
 end
